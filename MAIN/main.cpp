40d27ad6f1bb40e01d09bcdf726a9a19bb5f821f
taiQui <taiqui@localhost.localdomain>
2018-12-05 12:13:11 +0100

    test pour adrien

--- MAIN/main.cpp
+++ MAIN/main.cpp
@@ -1,5 +1,5 @@
 #include "main.hpp"
 
 int main(){
-  cout << "coucou" << endl << "Adrien tu fais chier" << endl;
+  cout << "coucou" << endl << "Adrien tu fais chier" << endl << "adrien t'es moche"<<endl;
 }
